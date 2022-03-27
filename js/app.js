faac23557a2038c1c0059798cdd689b5ad71b60f
Fatih Turhan <m.fatihturhan@gmail.com>
2022-03-27 23:16:17 +0300

    random numbers created

---
+++ js/app.js
@@ -0,0 +1,34 @@
+const container = document.querySelector(".game-center");
+const btns = document.querySelectorAll(".number-btn");
+const value = document.querySelector(".value");
+
+let initialValue = 1;
+
+value.textContent = initialValue;
+const emptyList = [];
+
+for (let i = 0; i < 25; i++) {
+  const randomNum = Math.ceil(Math.random() * 25);
+  if (!emptyList.includes(randomNum)) {
+    emptyList.push(randomNum);
+  } else {
+    i = i - 1;
+  }
+}
+container.innerHTML = emptyList
+  .map((item) => {
+    return `<button class="number-btn">
+          <p class="number">${item}</p>
+        </button>`;
+  })
+  .join("");
+
+container.addEventListener("click", (e) => {
+  const selected = Number(e.target.textContent);
+  if (initialValue === selected) {
+    initialValue++;
+    console.log(initialValue);
+    value.textContent = initialValue;
+    e.target.style.color;
+  }
+});
